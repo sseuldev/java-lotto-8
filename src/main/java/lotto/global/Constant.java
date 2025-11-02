e82f2d4075c6f14b0d2977fd39333367b3694c13
sseuldev <lhs010107@gmail.com>
2025-11-03 00:53:47 +0900

    feat: 공통 상수 클래스 구현

---
+++ src/main/java/lotto/global/Constant.java
@@ -0,0 +1,9 @@
+package lotto.global;
+
+public class Constant {
+    public static final int LOTTO_NUMBERS_LENGTH = 6;
+    public static final int MINIMUM_LOTTO_NUMBER = 1;
+    public static final int MAXIMUM_LOTTO_NUMBER = 45;
+    private static final int REFERENCE_DIVISION_NUMBER = 1000;
+    private static final String DELIMITER = ",";
+}
