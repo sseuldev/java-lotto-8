2c735a676726fcbb8ea3fabd769d4e9dfaaeabce
sseuldev <lhs010107@gmail.com>
2025-11-03 06:25:12 +0900

    feat: Application main 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView input = new InputView();
+        OutputView output = new OutputView();
+
+        LottoController lottoController = new LottoController(input, output);
+        lottoController.run();
     }
 }
