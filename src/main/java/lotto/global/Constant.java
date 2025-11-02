d723e98625f382df42fa914c525439b2a4821f3b
sseuldev <lhs010107@gmail.com>
2025-11-03 01:28:35 +0900

    fix: 접근 지정자 오류 수정 기존 private으로 설정되어있던 지정자를 public으로 수정하였습니다.

--- src/main/java/lotto/global/Constant.java
+++ src/main/java/lotto/global/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
     public static final int LOTTO_NUMBERS_LENGTH = 6;
     public static final int MINIMUM_LOTTO_NUMBER = 1;
     public static final int MAXIMUM_LOTTO_NUMBER = 45;
-    private static final int REFERENCE_DIVISION_NUMBER = 1000;
-    private static final String DELIMITER = ",";
+    public static final int REFERENCE_DIVISION_NUMBER = 1000;
+    public static final String DELIMITER = ",";
 }
